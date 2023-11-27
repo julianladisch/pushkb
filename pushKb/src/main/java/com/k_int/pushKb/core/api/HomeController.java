ee6170a5b4788664ae0e51bbea7b2cba08312609
Ethan Freestone <ethan.freestone@k-int.co.uk>
2023-11-27 16:56:38 +0000

    feat: Security first attempt
    Added in some KC security stuff, seeing how it goes

---
+++ pushKb/src/main/java/com/k_int/pushKb/core/api/HomeController.java
@@ -0,0 +1,21 @@
+package com.k_int.pushKb.core.api;
+
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+import io.micronaut.security.annotation.Secured;
+import io.micronaut.security.rules.SecurityRule;
+//import io.micronaut.views.View;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+public class HomeController {
+  // TODO figure out what's happening here
+  @Secured(SecurityRule.IS_ANONYMOUS)
+  //@View("home")
+  @Get 
+  public Map<String, Object> index() {
+      return new HashMap<>();
+  }
+}
