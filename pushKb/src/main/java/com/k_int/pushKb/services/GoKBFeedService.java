0c02cd6382df1113237a151795cfb48ab0c9a0ed
Ethan Freestone <ethan.freestone@k-int.co.uk>
2023-11-28 16:09:08 +0000

    feat: GoKB Fetch
    Initial fetch of GoKB Data on scheduled basis

---
+++ pushKb/src/main/java/com/k_int/pushKb/services/GoKBFeedService.java
@@ -0,0 +1,56 @@
+package com.k_int.pushKb.services;
+
+
+import java.net.URI;
+import java.time.Instant;
+
+import jakarta.inject.Singleton;
+
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Flux;
+// HTTP STUFFS
+import io.micronaut.core.type.Argument;
+import io.micronaut.http.HttpRequest;
+import io.micronaut.http.client.HttpClient;
+import io.micronaut.http.client.annotation.Client;
+import io.micronaut.http.uri.UriBuilder;
+import org.reactivestreams.Publisher;
+import org.reactivestreams.Subscriber;
+
+import static io.micronaut.http.HttpHeaders.ACCEPT;
+import static io.micronaut.http.HttpHeaders.USER_AGENT;
+
+@Slf4j
+@Singleton
+public class GoKBFeedService {
+  // FIXME we need a GoKB Client class
+  private final HttpClient httpClient;
+  private final URI uri;
+  public GoKBFeedService(@Client(id = "gokb") HttpClient httpClient) {  
+        this.httpClient = httpClient;
+        this.uri = UriBuilder.of("https://gokb.org")
+          .path("gokb")
+          .path("api")
+          .path("scroll")
+          .queryParam("component_type", "Package")
+          //.queryParam("changedSince", null) // TODO this won't be null mostly
+          //.queryParam("status", "Current") // I forget if we need this
+          .build();
+    }
+
+  public void testScheduling() {
+    log.info("LOGDEBUG RAN AT: {}", Instant.now());
+    Flux<String> flux = Flux.from(fetchReleases());
+    flux.subscribe(thing -> {
+      log.info("LOGDEBUG LOGGING OUT TEST: {}", thing);
+    });
+	}
+
+  public Publisher<String> fetchReleases() {
+    log.info("WHAT IS URI?: {}", uri);
+    HttpRequest<?> req = HttpRequest.GET(uri) 
+            .header(USER_AGENT, "Micronaut HTTP Client")
+            .header(ACCEPT, "application/json"); 
+    return httpClient.retrieve(req); 
+  }
+}
---
+++ pushKb/src/main/java/com/k_int/pushKb/services/SchedulingService.java
@@ -0,0 +1,21 @@
+package com.k_int.pushKb.services;
+
+import io.micronaut.scheduling.annotation.Scheduled;
+import jakarta.inject.Singleton;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Singleton
+public class SchedulingService {
+	private final GoKBFeedService goKBFeedService;
+
+	public SchedulingService(GoKBFeedService goKBFeedService) {
+		this.goKBFeedService = goKBFeedService;
+	}
+
+  // FIXME need to work on delay here
+  @Scheduled(initialDelay = "1s", fixedDelay = "1h")
+	public void scheduledTask() {
+    goKBFeedService.testScheduling();
+	}
+}
