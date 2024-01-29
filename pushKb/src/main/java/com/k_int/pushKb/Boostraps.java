fd7ec2f6ee6e5a181ac2a23c3eed50e6e30b8498
Ethan Freestone <ethan.freestone@k-int.co.uk>
2024-01-29 14:16:17 +0000

    refactor: Bootstrap
    Refactor Bootstrap file to use Maps instead of nested interfaces, so we can iterate over those with Types in hand

--- pushKb/src/main/java/com/k_int/pushKb/Boostraps.java
+++ pushKb/src/main/java/com/k_int/pushKb/Boostraps.java
@@ -1,33 +1,48 @@
 package com.k_int.pushKb;
 
 import java.time.Instant;
+import java.util.AbstractMap;
+import java.util.Map;
 
 import com.k_int.pushKb.destinations.folio.FolioDestination;
+import com.k_int.pushKb.model.Destination;
 import com.k_int.pushKb.model.DestinationSourceLink;
 import com.k_int.pushKb.model.PushTask;
+import com.k_int.pushKb.model.Source;
 import com.k_int.pushKb.model.SourceType;
 import com.k_int.pushKb.sources.gokb.GokbSource;
 
 public interface Boostraps {
-	public static interface Sources {
-		public static final GokbSource GOKB_PACKAGE = GokbSource.builder()
-                                              .sourceUrl("https://gokb.org/gokb/api")
-                                              .sourceType(SourceType.PACKAGE)
-                                              .build();
-    public static final GokbSource GOKB_TIPP = GokbSource.builder()
-                                            .sourceUrl("https://gokb.org/gokb/api")
-                                            .sourceType(SourceType.TIPP)
-                                            .build();
-	}
 
-  public static interface Destinations {
-		public static final FolioDestination LOCAL_RANCHER_FOLIO = FolioDestination.builder()
-                                            .destinationUrl("http://localhost:8080")
-                                            .tenant("test1")
-                                            .build();
-	}
+  public static final Map<String, Source> sources = Map.ofEntries(
+    new AbstractMap.SimpleEntry<String, GokbSource>(
+      "GOKB_PACKAGE",
+      GokbSource.builder()
+        .sourceUrl("https://gokb.org/gokb/api")
+        .sourceType(SourceType.PACKAGE)
+        .build()
+    ),
+    new AbstractMap.SimpleEntry<String, GokbSource>(
+      "GOKB_TIPP",
+      GokbSource.builder()
+        .sourceUrl("https://gokb.org/gokb/api")
+        .sourceType(SourceType.TIPP)
+        .build()
+    )
+  );
 
-  public static interface DestinationSourceLinks {
+  public static final Map<String, Destination> destinations = Map.ofEntries(
+    new AbstractMap.SimpleEntry<String, Destination>(
+      "LOCAL_RANCHER_FOLIO",
+      FolioDestination.builder()
+        .destinationUrl("http://localhost:8080")
+        .tenant("test1")
+        .build()
+    )
+  );
+
+  // FIXME work on these later
+/*   public static interface DestinationSourceLinks {
 		public static final DestinationSourceLink FOLIO_GOKB_TIPP = DestinationSourceLink.builder()
                                             .transform("example_tranform")
                                             .source(Sources.GOKB_TIPP)
@@ -36,7 +51,7 @@ public interface Boostraps {
                                             .lastSentPointer(Instant.EPOCH)
                                             .footPointer(Instant.EPOCH)
                                             .build();
-	}
+	} */
 
   // This might be tricky now they aren't direct lookups
 /*   public static interface PushTasks {
