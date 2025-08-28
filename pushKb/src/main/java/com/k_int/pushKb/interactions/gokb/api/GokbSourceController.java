3e5d0fddfe24027000d0ced2ec7c57570a584327
Ethan Freestone <ethan.freestone@k-int.co.uk>
2025-08-28 16:57:52 +0100

    fix: Keycloak authentication BS

--- pushKb/src/main/java/com/k_int/pushKb/interactions/gokb/api/GokbSourceController.java
+++ pushKb/src/main/java/com/k_int/pushKb/interactions/gokb/api/GokbSourceController.java
@@ -1,18 +1,25 @@
 package com.k_int.pushKb.interactions.gokb.api;
 
+import io.micronaut.http.MediaType;
+import io.micronaut.http.annotation.Body;
 import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Post;
 import io.micronaut.security.annotation.Secured;
 import io.micronaut.security.rules.SecurityRule;
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 
 import com.k_int.pushKb.crud.CrudControllerImpl;
 import com.k_int.pushKb.interactions.gokb.model.GokbSource;
 import com.k_int.pushKb.interactions.gokb.storage.GokbSourceRepository;
+import org.reactivestreams.Publisher;
 
 @Controller("/sources/gokbsource")
 @Slf4j
 @Secured(SecurityRule.IS_AUTHENTICATED)
 public class GokbSourceController extends CrudControllerImpl<GokbSource> {
+	GokbSourceRepository repository;
+
   public GokbSourceController(GokbSourceRepository repository) {
     super(repository);
   }
@@ -23,4 +30,14 @@ public class GokbSourceController extends CrudControllerImpl<GokbSource> {
 
     // TODO we may need an API way to clear the sourceRecord queue for a given source...
     // as above maybe GenericSourceController or something
+
+	// We need to use ensureSource here to make sure that side effects happen as expected
+	// Such as DutyCycleTask creation etc.
+	@Override
+	@Post(uri = "/", produces = MediaType.APPLICATION_JSON)
+	public Publisher<GokbSource> post(
+		@Valid @Body GokbSource src
+	) {
+		return repository.ensureSource(src);
+	}
 }
